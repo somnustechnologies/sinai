2779e8f5a56c9fbe63226ff91b2a5fc261017736
DeliriumTremens <daniel.castillo00@gmail.com>
2014-06-03 13:34:25 -0500

    Carga Inicial

---
+++ sinai-common/src/main/java/gob/df/sds/sinai/common/App.java
@@ -0,0 +1,13 @@
+package gob.df.sds.sinai.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
